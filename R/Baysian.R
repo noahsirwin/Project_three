c4df4fdb081a08216a659a3aee57c0128c2471c6
Noah Irwin <noah.irwin@selu.edu>
2024-11-06 18:02:56 +0000

    Commiting and pushing two other functions

---
+++ R/Baysian.R
@@ -0,0 +1,12 @@
+This function will run either the mixdist/mclust function on my Snout girdle length measurement data
+The purpose is to find out rather my data is unimodal/bimodal/trimodal using a best fit model
+data frame  column 1    column 2     column 3
+
+
+data frame:excell Sheet for a single site and collection trip ex. athens_sgl_1
+
+column 1: Larval_frequency_sgls
+
+coulmn 2: meta_frequency_sgls
+
+column 3: adult_frequency_sgls
---
+++ R/Map.R
@@ -0,0 +1,5 @@
+Creating a map using Lat and Lon
+
+This function will be able to create a map from an excell sheet containing two columns and the names of my 7 sites
+column 1  column 2     column 3
+Latitude  Longitude   Site.Name
